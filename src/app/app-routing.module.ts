fcaee2de6ae7778baff0da3b15fde6401c73ac02
Almariodelacuesta <108443435+Almariodelacuesta@users.noreply.github.com>
2022-08-06 02:04:44 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { LoginPage } from './login/login.page';
+import { RegisterPage } from './register/register.page';
 
 const routes: Routes = [
   {
@@ -19,6 +22,18 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: '', redirectTo:'login', pathMatch: 'full'
+  },
+  {
+    path: 'login', component : LoginPage
+  },
+  {
+    path: 'dashboard', component : DashboardComponent
+  },
+  {
+    path: 'register', component : RegisterPage
+  }
 ];
 
 @NgModule({
